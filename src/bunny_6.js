e19382e8350ebbf7fc07d7d8f92b091a94f596ee
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-05-21 16:16:05 +0200

    feat(geometry): experimenting with basic geometry support (tris), and to check if non nested arrays as data sources are valid

--- src/bunny_6.js
+++ src/bunny_6.js
@@ -1,25 +1,33 @@
 var regl = require('regl')()
 import {identity, perspective, lookAt} from 'gl-mat4'
+import mat4 from 'gl-mat4'
 const {frame, clear, buffer, elements} = regl
 import bunny from 'bunny'
 //import glslify from 'glslify'
 var glslify = require('glslify')
 
+const positions = [
+  2, 2, 0,
+  1, 2, -2,
+  0, 1, -2,
+  ]
+let objMat = mat4.identity([])//create([])
+mat4.translate(objMat, objMat, [1, 10, 20])
 
-const camSpeed = 1
+const camSpeed = 5.2
 
 const drawBunny = regl({
   vert: glslify(__dirname + '/shaders/base.vert'),
   frag: glslify(__dirname + '/shaders/base.frag'),
 
   attributes: {
-    position: buffer(bunny.positions)
+    position: buffer(positions)
   },
-
-  elements: elements(bunny.cells),
+  count: 3,
+  // elements: elements(bunny.cells),
 
   uniforms: {
-    model: identity([]),
+    model: objMat,//identity([]),
     view: (args, batchId, stats) => {
       const t = 0.01 * stats.count * 1 / camSpeed
       return lookAt([],
@@ -35,6 +43,7 @@ const drawBunny = regl({
   }
 })
 
+// do the drawing
 frame(() => {
   clear({
     depth: 1,
@@ -43,3 +52,18 @@ frame(() => {
 
   drawBunny()
 })
+
+/*view: (props, context) => {
+      var t = 0.01 * context.count
+      return mat4.lookAt([],
+        [30 * Math.cos(t), 2.5, 30 * Math.sin(t)],
+        [0, 2.5, 0],
+        [0, 1, 0])
+    },
+    projection: (props, context) => {
+      return mat4.perspective([],
+        Math.PI / 4,
+        context.viewportWidth / context.viewportHeight,
+        0.01,
+        1000)
+    }*/
