59b3b90d34cfa351a4a45190c00bf3413fe9020d
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-05-22 21:37:47 +0200

    feat(proto1:transforms): removed need to create mat4 manually for models to display: - transformation matrix now created from pos,rot,scale infos & used to display models - simpiified input data structures accordingly - 'hackish' support for z-up - experimented with batched rendering with different transforms: VERY promising

--- src/proto1/model.js
+++ src/proto1/model.js
@@ -1,16 +1,15 @@
 var regl = require('regl')()
-import {identity, perspective, lookAt} from 'gl-mat4'
+import { identity, perspective, lookAt } from 'gl-mat4'
 import mat4 from 'gl-mat4'
 const {frame, clear, buffer, elements, prop} = regl
 import bunny from 'bunny'
-//import glslify from 'glslify'
+// import glslify from 'glslify'
 var glslify = require('glslify')
 
+const camSpeed = 1.2
 
-const camSpeed = 0.2
-
-function drawModel(data) {
-  //const {positions, cells, mat, color, pos} = data
+function drawModel (data) {
+  // const {positions, cells, mat, color, pos} = data
   const {geometry, transforms} = data
   let params = {
     vert: glslify(__dirname + '/shaders/base.vert'),
@@ -20,7 +19,7 @@ function drawModel(data) {
       position: buffer(geometry.positions)
     },
     uniforms: {
-      model: transforms.mat,
+      model: prop('mat'),//transforms.mat,
       view: (props, context) => {
         const t = 0.01 * context.count * camSpeed
         return mat4.lookAt([],
@@ -48,47 +47,73 @@ function drawModel(data) {
     }
   }
   if (geometry.cells) {
-    //params = Object.assign(params, {elements: elements(cells)})
     params.elements = elements(geometry.cells)
-  }
-  else{
-    params.count = geometry.positions.length/3
+    //console.log(geometry.cells.length*3)
+  } else {
+    params.count = geometry.positions.length / 3
   }
   const {pos, rot, sca} = data.transforms
   const {color} = data
-  return regl(params)({pos, color})
+
+  //create transform matrix
+  let modelMat = mat4.identity([])
+  mat4.translate(modelMat, modelMat, [pos[0], pos[2], pos[1]])//z up
+  //mat4.rotate(modelMat, modelMat, rad:Number, axis:vec3)
+  mat4.scale(modelMat, modelMat, [sca[0], sca[2], sca[1]])
+
+  //test for batches
+  /*let batches = []
+  for(var i=0;i<100;i++){
+    let modelMat = mat4.identity([])
+    mat4.translate(modelMat, modelMat, [pos[0]+i*2+2, pos[2], pos[1]])//z up
+    mat4.scale(modelMat, modelMat, [sca[0], sca[2], sca[1]])
+
+    batches.push({pos, color, mat: modelMat})
+  }
+  return regl(params)(batches)
+  */
+  return regl(params)({pos, color, mat: modelMat})
 }
-///per model data
-let triMat = mat4.identity([])// create([])
+
+/* //////////////// */
+// /per model data
+let triMat = mat4.identity([]) // create([])
 mat4.translate(triMat, triMat, [1, 10, 20])
 
-const triData = {
-  geometry:{
-    positions :[
+const triData = {
+  geometry: {
+    positions: [
       2, 2, 0,
       1, 2, -2,
       0, 1, -2
     ],
   },
-  transforms:{
-    pos:[0,0,0],
-    rot:[0,0,0],
-    sca:[1,1,1],
-    mat:triMat
+  transforms: {
+    pos: [0, 0, 0],
+    rot: [0, 0, 0],
+    sca: [1, 1, 1]
   },
-  color:[1,0,0,0.5]
+  color: [1, 0, 0, 0.5]
 }
 
-let bunnyMat = mat4.identity([])// create([])
 const bunnyData = {
-  geometry:bunny,
-  transforms:{
-    pos:[0 ,0 ,0],
-    rot:[0 ,0 ,0],
-    sca:[1 ,1 ,1],
-    mat:bunnyMat
+  geometry: bunny,
+  transforms: {
+    pos: [0, 0, 0],
+    rot: [0, 0, 0],
+    sca: [1, 1, 1]
   },
-  color:[0,1,0,0.5]
+  color: [0, 1, 0, 0.5]
+}
+
+const bunnyData2 = {
+  geometry: bunny,
+  transforms: {
+    pos: [0, 0, 0],
+    rot: [0, 0, 0],
+    sca: [-1, 1, -1]
+  },
+  color: [0, 1, 1, 0.5]
 }
 
 // do the drawing
@@ -99,14 +124,31 @@ frame((props, context) => {
     color: [1, 1, 1, 1]
   })
 
-  const color =   [
-      Math.sin(0.001 * count),
-      Math.cos(0.02 * count),
-      Math.sin(0.3 * count),
-      1
-    ]
+  const color = [
+    Math.sin(0.001 * count),
+    Math.cos(0.02 * count),
+    Math.sin(0.3 * count),
+    1
+  ]
 
   drawModel(triData)
   drawModel(bunnyData)
-  //drawModel({positions: bunny.positions, cells: bunny.cells, mat: bunnyMat, color})
+  drawModel(bunnyData2)
+
+
+  /*this is slow, see batches example above for really speedy batching
+  for(var i=0;i<20;i++){
+    const bunnyDataT = {
+      geometry: bunny,
+      transforms: {
+        pos: [i*2+2, 0, -5],
+        rot: [0, 0, 0],
+        sca: [-1, 1, -1]
+      },
+      color
+    }
+    drawModel(bunnyDataT)
+  }*/
+
+// drawModel({positions: bunny.positions, cells: bunny.cells, mat: bunnyMat, color})
 })
