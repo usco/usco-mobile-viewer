dcaacaeb9f672e3218d8c7e0274b934ac1f9af4b
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-06-06 22:53:12 +0200

    feat(exp selection): experimental selection basics based on picking experiment

--- src/common/pickLoop.js
+++ src/common/pickLoop.js
@@ -12,7 +12,6 @@ export default function pickLoop (fullData) {
     console.log('mouse-change', fullData, buttons, x, y, mods)
 
     // Picking
-
     let screenWidth = window.innerWidth
     let screenHeight = window.innerHeight
 
@@ -37,15 +36,14 @@ export default function pickLoop (fullData) {
     mouse = [x, y] // screenHeight -
     viewport = [ 0, 0, screenWidth, screenHeight ]
 
-    // warning !!! issues with camera-unproject , itself used in other modules https://github.com/Jam3/camera-unproject/issues/1
+    // warning !!! posible issues with camera-unproject , itself used in other modules https://github.com/Jam3/camera-unproject/issues/1
 
     // store result in ray (origin, direction)
     pick(ray.ro, ray.rd, mouse, viewport, invProjView)
-    // pick(origin, direction, point, viewport, invProjView)
     const center = [0, 0, 0]
     const radius = 1.5
 
-    fullData.entities.forEach(function (entity) {
+    fullData.entities.map(function (entity, index) {
       // FIXME: do this only once, and not here
       const {pos, rot, sca} = entity.transforms
       let modelMat = mat4.identity([])
@@ -55,14 +53,23 @@ export default function pickLoop (fullData) {
       mat4.rotateZ(modelMat, modelMat, rot[1])
       mat4.scale(modelMat, modelMat, [sca[0], sca[2], sca[1]])
 
+      // convert min & max to world coordinates
       let min = vec3.transformMat4(mat4.identity([]), entity.bounds.min, modelMat) // out:vec3, a:vec3, m:mat4
       let max = vec3.transformMat4(mat4.identity([]), entity.bounds.max, modelMat) // out:vec3, a:vec3, m:mat4
 
       const bounds = [min, max] // [entity.bounds.min, entity.bounds.max] // FIXME !!!! bounds are in local coordinates, not world coordinates !
 
       const hit = intersect([], ray.ro, ray.rd, bounds) // intersect([], ray.ro, ray.rd, center, radius)
-      console.log('hit', hit)
+      let hitResult = null
+      if(hit){
+        hitResult = {hit, entity, index}
+        // just for testing 'pseudo' selection
+        entity.selected = ! entity.selected
+      }
+      return hitResult
     })
+    .filter(h=> h !== null )
+    .forEach(hit=>console.log('hit', hit.entity.id))
 
     function pickStuff () {
       // first check aabb && sphere
