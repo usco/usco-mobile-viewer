3746cd1968dec7f4a4d8c439fa8b981d446e379b
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-06-16 13:15:07 +0200

    refactor(camera): extracted camera state data from controls to camera module

--- src/common/camera.js
+++ src/common/camera.js
@@ -1,6 +1,19 @@
 const camera = {
   projection: new Float32Array(16),
-  view: new Float32Array(16)
+  view: new Float32Array(16),
+
+  thetaDelta: 0,
+  phiDelta: 0,
+  scale: 1,
+
+  position: [45, 0, 20],
+  target: [0, 0, 0]
 }
+
+// below this, dynamic stuff mostly, since this is also the ouput of the controls function
+/*camera: {
+
+  //view: mat4.create() // default, this is just a 4x4 matrix
+}*/
 
 export default camera
