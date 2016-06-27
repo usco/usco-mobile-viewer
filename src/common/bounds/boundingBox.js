320792b87c6d1a3a97d05b0f0637d5fc3227db93
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-06-27 13:34:28 +0200

    feat(tests - bounds): added unit test system and very basic tests for bounds computation - fix lots of issues with the implementations for bounding box/sphere

--- src/common/bounds/boundingBox.js
+++ src/common/bounds/boundingBox.js
@@ -5,6 +5,7 @@ export default function boundingBox (positions) {
   }
 
   const nested = (Array.isArray(positions) && Array.isArray(positions[0]))
+
   const dimensions = nested ? positions[0].length : 3
   let min = new Array(dimensions)
   let max = new Array(dimensions)
@@ -14,13 +15,23 @@ export default function boundingBox (positions) {
     max[i] = -Infinity
   }
 
-  positions.forEach(function (position) {
-    for (var i = 0; i < dimensions; i += 1) {
-      const _position = nested ? position[i] : position
-      max[i] = _position > max[i] ? _position : max[i] // position[i] > max[i] ? position[i] : max[i]
-      min[i] = _position < min[i] ? _position : min[i] // min[i] = position[i] < min[i] ? position[i] : min[i]
+  if (nested) {
+    positions.forEach(function (position) {
+      for (var i = 0; i < dimensions; i += 1) {
+        const _position = nested ? position[i] : position
+        max[i] = _position > max[i] ? _position : max[i] // position[i] > max[i] ? position[i] : max[i]
+        min[i] = _position < min[i] ? _position : min[i] // min[i] = position[i] < min[i] ? position[i] : min[i]
+      }
+    })
+  } else {
+    for (var j = 0; j < positions.length; j += dimensions) {
+      for (var i = 0; i < dimensions; i += 1) {
+        const _position = positions[i + j] // nested ? positions[i] : position
+        max[i] = _position > max[i] ? _position : max[i] // position[i] > max[i] ? position[i] : max[i]
+        min[i] = _position < min[i] ? _position : min[i] // min[i] = position[i] < min[i] ? position[i] : min[i]
+      }
     }
-  })
+  }
 
   return [min, max]
 }
--- src/common/bounds/computeBounds.js
+++ src/common/bounds/computeBounds.js
@@ -1,7 +1,7 @@
 import vec3 from 'gl-vec3'
 
 import boundingBox from './boundingBox'
-import {boundingSphereFromBoundingBox} from './boundingSphere'
+import boundingSphere from './boundingSphere'
 /* function to compute all bounding data given geometry data + position
 
 returns an object in this form:
@@ -14,14 +14,13 @@ bounds: {
 
 */
 export default function computeBounds (object) {
-
   const bbox = boundingBox(object.geometry.positions)
-  const bsph = boundingSphereFromBoundingBox(bbox)
   const center = vec3.scale(vec3.create(), vec3.add(vec3.create(), bbox[0], bbox[1]), 0.5)
+  const bsph = boundingSphere(center, object.geometry.positions)
 
   return {
     dia: bsph,
-    center,
+    center: [...center],
     min: bbox[0],
     max: bbox[1]
   }
