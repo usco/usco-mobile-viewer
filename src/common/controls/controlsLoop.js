e9e7b596abf6abf34aefb0079892e32584202cd3
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-08-30 10:59:50 +0200

    refactor(controlsLoop): cleaned up controlsLoop module

--- src/common/controls/controlsLoop.js
+++ src/common/controls/controlsLoop.js
@@ -12,11 +12,6 @@ export function controlsLoop (interactions, cameraData, fullData) {
   const {settings, camera} = cameraData
   const {gestures} = interactions
 
-  //gestures.taps.taps$.forEach(e=>console.log('taps',e))
-  //gestures.taps.shortSingleTaps$.forEach(e => console.log('shortSingleTaps', e))
-  //gestures.taps.shortDoubleTaps$.forEach(e => console.log('shortDoubleTaps', e))
-  //gestures.taps.longTaps$.forEach(e => console.log('longTaps', e))
-
   const rate$ = animationFrames() //heartBeat
   //const heartBeat$ = most.periodic(16, 'x')
   //sample(world, rate)
@@ -69,9 +64,8 @@ export function controlsLoop (interactions, cameraData, fullData) {
     return cameraState$
     return most.merge(
       cameraState$.take(2),
-      cameraState$// .throttle(20)
+      cameraState$
     )
-    // .map(cameraState => update(settings, cameraState))
   }
 
   function updateCompleteState (cameraState) {
@@ -88,75 +82,8 @@ export function controlsLoop (interactions, cameraData, fullData) {
     .merge(cameraState$)
     .map(updateCompleteState)
     //.tap(e=>console.log('here'))
-
-
-
    /*const updateForRender$ = most.sample(updateCompleteState, heartBeat$, cameraState$)
      .filter(x => x.changed)
      .map(updateCompleteState)
    return updateForRender$*/
-}
-
-
-/*const cameraState$ = most.combine(function (angles, zooms) {
-  return {angles, zooms}
-}, dragMoves$, zooms$)
-  .scan(function (state, current) {
-    const {angles, zooms} = current
-    // console.log('delta', angles)
-
-    let cameraState = update(settings, camera)
-    cameraState = zoom(settings, cameraState, zooms) // mutating, meh
-    cameraState = rotate(settings, cameraState, angles)
-    cameraState = update(settings, cameraState)
-
-    let data = fullData
-    data.camera = cameraState
-    return data
-  }, undefined)
-  .filter(x => x !== undefined)*/
-
-export function controlsLoopOld (cameraDefaults, render, fullData) {
-  const {settings, camera} = cameraDefaults
-  // FIXME: hack for now
-  let cameraState = update(settings, camera)
-  let prevMouse = [0, 0]
-
-  function onMouseChange (buttons, x, y, mods) {
-    // console.log('mouse-change', buttons, x, y, mods)
-    if (buttons === 1) {
-      let delta = [x - prevMouse[0], y - prevMouse[1]]
-      let angle = [0, 0]
-      angle[0] = 2 * Math.PI * delta[0] / 1800 * 2.0
-      angle[1] = -2 * Math.PI * delta[1] / 1800 * 2.0
-
-      cameraState = rotate(settings, cameraState, angle)
-    }
-    prevMouse = [x, y]
-  }
-
-  function onMouseWheel (dx, dy) {
-    const zoomDelta = dy
-    cameraState = zoom(settings, cameraState, zoomDelta)
-  }
-
-  function updateStep () {
-    cameraState = update(settings, cameraState)
-
-    if (cameraState && cameraState.changed) {
-      let data = fullData
-      data.camera = cameraState
-      render(data)
-    }
-    window.requestAnimationFrame(updateStep)
-  }
-
-  require('mouse-change')(onMouseChange)
-  require('mouse-wheel')(onMouseWheel)
-
-  let data = fullData
-  data.camera = cameraState
-  render(data)
-
-  requestAnimationFrame(updateStep)
 }
