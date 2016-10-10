fdb72aba44f567554c89465ce0db2540f5601b11
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-10-10 16:27:17 +0200

    feat(streaming xhr): experimenting with streaming 'xhr' apis & modules

--- src/proto1-basics/loader.js
+++ src/proto1-basics/loader.js
@@ -1,17 +1,21 @@
 import XhrStream from 'xhr-stream'
 import makeStlStream from 'usco-stl-parser'
+//import http from 'stream-http'
+import fetchStream from 'fetch-readablestream'
 import concat from 'concat-stream'
 
 const Duplex = require('stream').Duplex
+const Readable = require('stream').Readable
 
-export default function loadTest(){
+export default function loadTest(uri){
   console.log('trying to load')
   const xhr = new XMLHttpRequest()
-  xhr.open('GET', '/data/cube.stl', true)
-  //oReq.responseType = "arraybuffer"
+  //xhr.responseType = 'arraybuffer'
+  xhr.open('GET', uri, true)
 
   const xhrStream = new XhrStream( xhr )
-  const stlStream = makeStlStream({useWorker: false})
+
+  const stlStream = makeStlStream({useWorker: true})
 
   const foo = new Duplex({
     read(){
@@ -23,7 +27,25 @@ export default function loadTest(){
     }
   })
 
+  try{
+  //const xhrStream2 = http.get(uri)
+/*  http.get(uri, function onResponse(response) {
+    response.pipe(foo)
+})*/
+  //xhrStream2.pipe(foo)
+  console.log(fetchStream(uri))//.pipe(foo)
+}catch(error){
+  console.log('error',error)
+}
+
   return (xhrStream
     .pipe(stlStream))
 
 }
+
+
+/*  var oReq = new XMLHttpRequest()
+  oReq.responseType = 'arraybuffer'
+  oReq.addEventListener('load', reqListener)
+  oReq.open('GET', testFile)
+  oReq.send()*/
