45701948adf0ce0ed94bb394736473cd7ff0d7b5
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-10-10 16:50:53 +0200

    refactor(tMatrixFromTransforms): cleaned up & simplified helper a bit

--- src/common/utils/computeTMatrixFromTransforms.js
+++ src/common/utils/computeTMatrixFromTransforms.js
@@ -9,12 +9,12 @@ export default function computeTMatrixFromTransforms (params) {
   }
   const {pos, rot, sca} = Object.assign({}, defaults, params)
   // create transform matrix
-  let modelMat = mat4.identity([])
-  mat4.translate(modelMat, modelMat, pos) // [pos[0], pos[2], pos[1]]// z up
-  mat4.rotateX(modelMat, modelMat, rot[0])
-  mat4.rotateY(modelMat, modelMat, rot[1])
-  mat4.rotateZ(modelMat, modelMat, rot[2])
-  mat4.scale(modelMat, modelMat, sca) // [sca[0], sca[2], sca[1]])
+  let transforms = mat4.identity([])
+  mat4.translate(transforms, transforms, pos) // [pos[0], pos[2], pos[1]]// z up
+  mat4.rotateX(transforms, transforms, rot[0])
+  mat4.rotateY(transforms, transforms, rot[1])
+  mat4.rotateZ(transforms, transforms, rot[2])
+  mat4.scale(transforms, transforms, sca) // [sca[0], sca[2], sca[1]])
 
-  return modelMat
+  return transforms
 }
