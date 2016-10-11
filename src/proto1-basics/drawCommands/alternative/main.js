f0e71b15ed6d6a92b620711470d1b00dbe20c1b8
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-10-11 10:05:36 +0200

    feat(drawing): added enclosures display + tweaks

--- src/proto1-basics/drawCommands/alternative/main.js
+++ src/proto1-basics/drawCommands/alternative/main.js
@@ -1,12 +1,17 @@
-import wrapperScope from './wrapperScope'
+import wrapperScope from './wrapperScope2'
 import { default as modelM } from '../../../common/utils/computeTMatrixFromTransforms'
+import makeDrawEncl from './drawEncl'
 
 let tick = 0
 
 export default function prepareRenderAlt (regl) {
   const _wrapperScope = wrapperScope(regl)
+
   const bg = [1, 1, 1, 1]
-  const model = modelM({sca: [1, 1, 1]})
+  const model = modelM({sca: [0.5, 0.5, 0.5]})
+
+  let drawEncl = makeDrawEncl(regl)
+
   let command = (props) => {
     const {camera, view, entities} = props
     _wrapperScope(props, (context) => {
@@ -14,7 +19,8 @@ export default function prepareRenderAlt (regl) {
         color: bg,
         depth: 1
       })
-      entities.map(e => e.visuals.draw({view, camera, color: [1, 0, 0, 1], model}))
+      entities.map(e => e.visuals.draw({view, camera, color: e.visuals.color, model}))
+      drawEncl(props)
     })
   }
 
