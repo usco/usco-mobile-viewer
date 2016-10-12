957cef90f82029fde654948d779ac8ccf41cfbbc
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-10-12 23:16:57 +0200

    feat(streaming binary http source): added basic wrapper around fetch for streaming binary source of data

--- src/proto1-basics/loader.js
+++ src/proto1-basics/loader.js
@@ -1,48 +1,71 @@
 import XhrStream from 'xhr-stream'
 import makeStlStream from 'usco-stl-parser'
-//import http from 'stream-http'
+//require("web-streams-polyfill/dist/polyfill.min.js")
+
 import fetchStream from 'fetch-readablestream'
-import concat from 'concat-stream'
+
 
 const Duplex = require('stream').Duplex
 const Readable = require('stream').Readable
 
-export default function loadTest(uri){
+export default function loadTest (uri) {
   console.log('trying to load')
   const xhr = new XMLHttpRequest()
-  //xhr.responseType = 'arraybuffer'
+  // xhr.responseType = 'arraybuffer'
   xhr.open('GET', uri, true)
 
-  const xhrStream = new XhrStream( xhr )
-
+  const xhrStream = new XhrStream(xhr)
   const stlStream = makeStlStream({useWorker: true})
 
   const foo = new Duplex({
-    read(){
+    read() {
       console.log('reading')
     },
-    write(chunk, encoding, next){
-      console.log('chunk', chunk)
+    write(chunk, encoding, next) {
+      console.log('chunk in foo', chunk)
       next(null, Buffer(chunk))
     }
   })
 
-  try{
-  //const xhrStream2 = http.get(uri)
-/*  http.get(uri, function onResponse(response) {
-    response.pipe(foo)
-})*/
-  //xhrStream2.pipe(foo)
-  console.log(fetchStream(uri))//.pipe(foo)
-}catch(error){
-  console.log('error',error)
+  class HttpSourceStream extends Readable {
+    constructor (uri) {
+      super()
+
+      let reader
+      const finish = () => { this.emit('finish')}
+      const end = () => { this.emit('end')}
+
+      const push = (data) => this.push(data)
+
+      const process = (data) => {
+        const { value, done } = data
+        //console.log('SOURCE chunk', data , value, done)
+        if (done) {
+          finish()
+          end()
+        } else {
+          push(Buffer(value))
+          return reader.read().then(process)
+        }
+      }
+      fetchStream(uri).then(function (response) {
+        reader = response.body.getReader()
+        reader.read().then(process)
+      }).catch(e => console.log('error reading', e))
+    // if (!this.push(chunk)){
+    // }
+    }
+
+    _read (size) {
+      //console.log('_read')
+      //signal the source that it can start again
+    }
+  }
+
+ return new HttpSourceStream(uri).pipe(makeStlStream({useWorker: true}))
+
+ //return (xhrStream.pipe(stlStream))
 }
-
-  return (xhrStream
-    .pipe(stlStream))
-
-}
-
 
 /*  var oReq = new XMLHttpRequest()
   oReq.responseType = 'arraybuffer'
