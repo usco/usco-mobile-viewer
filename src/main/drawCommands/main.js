8f53d822e16705870f2bba6e5a961fec352dc47a
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-10-24 21:28:50 +0200

    feat(printHeadShadow): experimenting with visuals of printhead shadow

--- src/main/drawCommands/main.js
+++ src/main/drawCommands/main.js
@@ -1,10 +1,14 @@
 import makeWrapperScope from './wrapperScope'
 import makeDrawEnclosure from './drawEnclosure'
+import makeDrawPrintheadShadow from './drawPrintheadShadow'
+
+import { default as _model } from '../../common/utils/computeTMatrixFromTransforms'
 
 export default function prepareRenderAlt (regl, params) {
   const wrapperScope = makeWrapperScope(regl)
   const drawEnclosure = makeDrawEnclosure(regl, params.machineParams)
   let tick = 0
+  //entity color for outOfBounds ? [0.15, 0.15, 0.15, 0.3]
 
   let command = (props) => {
     const {camera, view, entities, background} = props
@@ -14,7 +18,18 @@ export default function prepareRenderAlt (regl, params) {
         color: background,
         depth: 1
       })
-      entities.map(e => e.visuals.draw({view, camera, color: e.visuals.color, model: e.modelMat}))
+      entities.map(e => e.visuals.draw({view, camera, color:e.visuals.color , model: e.modelMat}))
+      entities.map(function (entity) {
+        const {pos} = entity.transforms
+        const offset = pos[2]-entity.bounds.size[2]*0.5
+        const model = _model({pos: [pos[0], pos[1], -0.1]})
+        const headSize = [100,60]
+        const width = entity.bounds.size[0]+headSize[0]
+        const length = entity.bounds.size[1]+headSize[1]
+
+        return makeDrawPrintheadShadow(regl, {width,length})({view, camera, model, color: [0.1, 0.1, 0.1, 0.15]})
+      })
+
       drawEnclosure(props)
     })
   }
