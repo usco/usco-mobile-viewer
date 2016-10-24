5f2ec36c1ed498861d2acfec78eb99cb25b773e9
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-10-24 10:07:23 +0200

    feat(android callbacks): added boilerplate of android callbacks

---
+++ src/common/android/androidInterface.js
@@ -0,0 +1,22 @@
+let mobileCaller = {
+  interfaceName: 'JAMDroid',
+
+  call: function (method) {
+    if (eval('typeof ' + this.interfaceName) !== 'undefined') {
+      new Function(this.interfaceName + '.' + method)()
+    }
+  }
+
+}
+
+export function onLoadModelError (error) {
+  mobileCaller.call('onLoadModel(false)')
+}
+
+export function onLoadModelSuccess (model) {
+  mobileCaller.call('onLoadModel(true)')
+}
+
+export function onBoundsExceeded (){
+  mobileCaller.call('onBoundsExceeded()')
+}
