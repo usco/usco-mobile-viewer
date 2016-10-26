109d416532d09e63e58aed7a82c313ea00779f4d
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-10-26 14:54:48 +0200

    feat(formatRawMachineData): added raw machine data formatter + test

---
+++ src/common/utils/formatRawMachineData.js
@@ -0,0 +1,10 @@
+export default function formatRawMachineData (rawData) {
+  return {
+    machine_volume: [
+      rawData.machine_width.default_value,
+      rawData.machine_depth.default_value,
+      rawData.machine_height.default_value],
+    machine_head_with_fans_polygon: rawData.machine_head_with_fans_polygon.default_value,
+    machine_disallowed_areas: rawData.machine_disallowed_areas.default_value
+  }
+}
