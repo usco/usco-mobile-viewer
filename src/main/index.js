2756d966be247424a0b83abcd4c45d540ac08192
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-10-27 15:36:35 +0200

    refactor(loader): improved streaming stl loader error handling - moved out observable creation into loader module - added handling of errors at all the steps in the loader 'mini pipeline' - a few related cleanups

--- src/main/index.js
+++ src/main/index.js
@@ -8,13 +8,10 @@ import prepareRender from './drawCommands/render'
 import { params as cameraDefaults } from '../common/controls/orbitControls'
 import camera from '../common/camera'
 
-import create from '@most/create'
-import { combine, combineArray, merge } from 'most'
+import { combine, combineArray, merge, just } from 'most'
 import limitFlow from '../common/utils/most/limitFlow'
-import callBackToStream from '../common/utils/most/callBackToStream'
 
 import loadAsStream from './loader'
-import { concatStream } from 'usco-stl-parser'
 
 // interactions
 import controlsStream from '../common/controls/controlsStream'
@@ -31,38 +28,12 @@ import { makeEntitiesModel, makeMachineModel } from './state'
 import { makeVisualState } from './visualState'
 
 // basic api
-import { onLoadModelError, onLoadModelSuccess, onBoundsExceeded, onViewerReady } from '../common/mobilePlatforms/interface'
+import makeInterface from '../common/mobilePlatforms/interface'
 import nativeApiDriver from './sideEffects/nativeApiDriver'
 
 // ////////////
+const {onLoadModelError, onLoadModelSuccess, onBoundsExceeded, onViewerReady, onMachineParamsError} = makeInterface()
 const nativeApi = nativeApiDriver()
-
-const modelUri$ = merge(
-  adressBarDriver,
-  nativeApi.modelUri$
-).multicast()
-
-const setMachineParams$ = merge(
-  nativeApi.machineParams$
-).multicast()
-
-const parsedSTL$ = modelUri$
-  .tap(e => console.log('modelUri', e))
-  .filter(x => x !== null)
-  .delay(200)
-  .flatMap(function (url) {
-    return create((add, end, error) => {
-      loadAsStream(url).pipe(concatStream(data => add(data)))
-    })
-  })
-  .tap(e => console.log('done loading'))
-  .flatMapError(function (error) {
-    console.error('ERROR in loading mesh file !!', error)
-    onLoadModelError(error)
-    return undefined
-  })
-  .filter(x => x !== undefined)
-  .multicast()
 
 const regl = reglM({
   extensions: [
@@ -80,16 +51,51 @@ const container = document.querySelector('canvas')
   - even if regl can 'combine' various uniforms, attributes, props etc, the rule above still applies
 */
 
+/////////
+const modelUri$ = merge(
+  adressBarDriver,
+  nativeApi.modelUri$
+)
+  .flatMapError(function (error) {
+    // console.log('error', error)
+    onLoadModelError(error)
+    return just(null)
+  })
+  .filter(x => x !== null)
+  .multicast()
+
+const setMachineParams$ = merge(
+  nativeApi.machineParams$
+)
+  .flatMapError(function (error) {
+    // console.log('error', error)
+    onMachineParamsError(error)
+    return just(null)
+  })
+  .filter(x => x !== null)
+  .multicast()
+
+const parsedSTL$ = modelUri$
+  .flatMap(loadAsStream)
+  .tap(e => console.log('done loading', e))
+  .flatMapError(function (error) {
+    onLoadModelError(error)
+    return just(undefined)
+  })
+  .filter(x => x !== undefined)
+  .multicast()
+
+
 // interactions : camera controls
 const baseInteractions$ = interactionsFromEvents(container)
 const gestures = pointerGestures(baseInteractions$)
 const camState$ = controlsStream({gestures}, {settings: cameraDefaults, camera})
 
 const render = prepareRender(regl)
-const addEntities$ = entityPrep(parsedSTL$, regl)
+const addEntities$ = entityPrep(parsedSTL$)
 
 const entities$ = makeEntitiesModel({addEntities: addEntities$})
-const machine$ = makeMachineModel({setMachineParams: setMachineParams$}, regl)
+const machine$ = makeMachineModel({setMachineParams: setMachineParams$})
 
 const appState$ = combineArray(
   function (entities, machine) {
@@ -117,27 +123,6 @@ addEntities$.forEach(m => onLoadModelSuccess()) // side effect => dispatch to ca
 boundsExceeded$.forEach(onBoundsExceeded) // dispatch message to signify out of bounds
 
 // for testing only
-/*const machineParams = {
-  machine_uuid: 'xx',
-  machine_volume: [213, 220, 350],
-  machine_disallowed_areas: [
-    [[-91.5, -115], [-115, -115], [-115, -104.6], [-91.5, -104.6]],
-    [[-99.5, -104.6], [-115, -104.6], [-115, 104.6], [-99.5, 104.6]],
-    [[-94.5, 104.6], [-115, 104.6], [-115, 105.5], [-94.5, 105.5]],
-    [[-91.4, 105.5], [-115, 105.5], [-115, 115], [-91.4, 115]],
-
-    [[77.3, -115], [77.3, -98.6], [115, -98.6], [115, -115]],
-    [[97.2, -98.6], [97.2, -54.5], [113, -54.5], [113, -98.6]],
-    [[100.5, -54.5], [100.5, 99.3], [115, 99.3], [115, -54.5]],
-    [[77, 99.3], [77, 115], [115, 115], [115, 99.3]]
-  ],
-  machine_head_with_fans_polygon: [
-    [ -40, 10 ],
-    [ -40, -30 ],
-    [ 60, 10 ],
-    [ 60, -30 ]
-  ]
-}*/
 const machineParams = {
   'machine_width': { 'default_value': 215 },
   'machine_depth': { 'default_value': 215 },
@@ -163,4 +148,4 @@ const machineParams = {
 }
 // for testing
 // informations about the active machine
-//window.nativeApi.setMachineParams(machineParams)
+window.nativeApi.setMachineParams(machineParams)
