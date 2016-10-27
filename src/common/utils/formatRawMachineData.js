fea05b4597cfa22c29916bc15bd74a279461856b
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-10-27 13:03:18 +0200

    feat(formatRawMachineData): updated formatRawMachineData to match latest api changes

--- src/common/utils/formatRawMachineData.js
+++ src/common/utils/formatRawMachineData.js
@@ -4,7 +4,8 @@ export default function formatRawMachineData (rawData) {
       rawData.machine_width.default_value,
       rawData.machine_depth.default_value,
       rawData.machine_height.default_value],
-    machine_head_with_fans_polygon: rawData.machine_head_with_fans_polygon.default_value,
-    machine_disallowed_areas: rawData.machine_disallowed_areas.default_value
+    machine_head_with_fans_polygon: [], // rawData.machine_head_with_fans_polygon.default_value,
+    machine_disallowed_areas: [], // rawData.machine_disallowed_areas.default_value
+    machine_printable_area: []
   }
 }
