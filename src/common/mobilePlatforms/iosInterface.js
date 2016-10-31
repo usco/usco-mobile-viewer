59bb3e79ba4c94ca3f97d30dd4db722341ed03d2
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-10-31 16:54:22 +0100

    feat(mobile app interfaces): - simplified mobile api call managements - boundsExceeded is now objectFitsPrintableVolume and gets fired even when not exceeded (with 'true') - lots of related cleanups

--- src/common/mobilePlatforms/iosInterface.js
+++ src/common/mobilePlatforms/iosInterface.js
@@ -1,31 +1,17 @@
-function IOSCallback (data) {
-  let formated = ''
-  const urlScheme = 'yoururlscheme'
-  const host = 'somehost'
-  Object.keys(data).forEach(k => {
-    formated += `${k}=${data[k]}`
-  })
-  const location = `${urlScheme}://${host}?${formated}`
-  console.log('here', location)
-  window.location = location
-}
-
 function callNativeApp (path, payload) {
   try {
-    console.log('calling native app', path)
+    //console.log('calling native app', path, payload)
     window.webkit.messageHandlers[path].postMessage(payload)
   } catch(err) {
-    console.log('Not native')
+    // console.log('Not native')
   }
 }
 
 export function makeIosInterface () {
   return {
-    onLoadModelError: () => callNativeApp('loadModel', 'error'),
-    onLoadModelSuccess: () => callNativeApp('loadModel', 'success'),
-    onBoundsExceeded: () => callNativeApp('objectBounds', 'exceeded'),
-    onViewerReady: () => callNativeApp('viewer', 'ready'),
-    onMachineParamsError: () => callNativeApp('machineParams', 'error'),
-    onMachineParamsSuccess:() => callNativeApp('machineParams','success')
+    viewerReady: () => callNativeApp('viewer', 'ready'),
+    modelLoaded: (value) => callNativeApp('loadModel', value ? 'success' : 'error'),
+    machineParamsLoaded: (value) => callNativeApp('machineParams', value ? 'success' : 'error'),
+    objectFitsPrintableVolume: (value) => callNativeApp('objectFitsPrintableVolume', `${value}`)
   }
 }
