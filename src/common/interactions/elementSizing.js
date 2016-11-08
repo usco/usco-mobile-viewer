2b2bdb7b4b7907d9f7eb3bf0c92f684c6c7a734a
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-11-08 15:58:06 +0100

    refactor(elementSizing): modified elementSizing to work better with canvas/gl elements

--- src/common/interactions/elementSizing.js
+++ src/common/interactions/elementSizing.js
@@ -1,15 +1,18 @@
 import { fromEvent } from 'most'
 
 // element resize event stream, throttled by throttle amount (250ms default)
-export function elementSize (element, throttle = 250) {
+export function elementSize (element, throttle = 25) {
+  const pixelRatio = window.devicePixelRatio
   function extractSize (x) {
-    x = x.target
-    const bRect = x.getBoundingClientRect ? x.getBoundingClientRect() : { left: 0, top: 0, bottom: 0, right: 0, width: 0, height: 0 }
-    return {width: x.innerWidth, height: x.innerHeight, aspect: x.innerWidth / x.innerHeight, bRect}
+    x = element
+    const width = Math.floor(element.clientWidth * pixelRatio)
+    const height = Math.floor(element.clientHeight * pixelRatio)
+    const bRect = element.getBoundingClientRect ? element.getBoundingClientRect() : { left: 0, top: 0, bottom: 0, right: 0, width: 0, height: 0 }
+    return {width, height, aspect: width / height, bRect}
   }
 
-  return fromEvent('resize', element)
+  return fromEvent('resize', window)//only window fires resize events...
     .throttle(throttle /* ms */)
     .map(extractSize)
-    //.startWith({width: element.innerWidth, height: element.innerHeight, aspect: element.innerWidth / element.innerHeight, bRect: undefined})
+    .startWith(extractSize())
 }
