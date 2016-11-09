6ca0520f2a99b1bdddbd5e482aa8e6d5e76569d8
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-11-09 14:31:48 +0100

    feat(zoom to fit): added correct offseting for zoom to fit

--- src/common/controls/controlsStream.js
+++ src/common/controls/controlsStream.js
@@ -1,16 +1,28 @@
 import { update, rotate, zoom, setFocus } from './orbitControls'
 import computeCameraToFitBounds from '../cameraEffects/computeCameraToFitBounds'
+import cameraOffsetToEntityBoundsCenter from '../cameraEffects/cameraOffsetToEntityBoundsCenter'
 
 import { model } from '../utils/modelUtils'
-import animationFrames from '../utils/animationFrames'
+import {animationFrames, rafStream} from '../utils/most/animationFrames'
 
 import mat4 from 'gl-mat4'
+import limitFlow from '../utils/most/limitFlow'
 
 export default function controlsStream (interactions, cameraData, focuses$, entityFocuses$, projection$) {
   const {settings, camera} = cameraData
   const {gestures} = interactions
 
-  const rate$ = animationFrames() // heartBeat
+  const rate$ = rafStream() // heartBeat
+
+  let i = 0
+  var newdiv = document.createElement("DIV")
+  newdiv.style.zIndex = 99
+  newdiv.style.position = 'absolute'
+  newdiv.style.color = 'red'
+  newdiv.style.top = '40px'
+  let textNode = document.createTextNode("some text"+i)
+  newdiv.appendChild(textNode)
+  document.body.appendChild(newdiv)
 
   const dragMoves$ = gestures.dragMoves
     // .throttle(16) // FIXME: not sure, could be optimized some more
@@ -43,60 +55,68 @@ export default function controlsStream (interactions, cameraData, focuses$, enti
         state.far)
       //state = Object.assign({}, state, {projection})
       state.projection = projection
-      state = Object.assign({}, state, update(settings, state)) // not sure
+      //state = Object.assign({}, state, update(settings, state)) // not sure
       return state
     }
 
     function applyRotation (state, angles) {
+      textNode.nodeValue= 'applyRotation'
+
       state = rotate(settings, state, angles) // mutating, meh
-      state = Object.assign({}, state, update(settings, state)) // not sure
+      //state = Object.assign({}, state, update(settings, state)) // not sure
       return state
     }
 
     function applyZoom (state, zooms) {
       // console.log('applyZoom', zooms)
+      textNode.nodeValue= 'applyZoom'
       state = zoom(settings, state, zooms) // mutating, meh
-      state = Object.assign({}, state, update(settings, state)) // not sure
+      //state = Object.assign({}, state, update(settings, state)) // not sure
       return state
     }
 
     function applyFocusOn (state, focuses) {
       state = setFocus(settings, state, focuses) // mutating, meh
-      state = Object.assign({}, state, update(settings, state)) // not sure
+      //state = Object.assign({}, state, update(settings, state)) // not sure
       return state
     }
 
     function zoomToFit (state, input) {
-      console.log('zoomToFit', input)
-      const {position, target} = computeCameraToFitBounds(state, input.bounds)
-      state.target = target
-      state.position = position
-      // state = update(settings, state) // not sure
-
-      const focuses = [input].map(function (nEntity) {
-        const mid = nEntity.bounds.max.map(function (pos, idx) {
-          return pos - nEntity.bounds.min[idx]
-        })
-        return mid
-      })[0]
-      state = setFocus(settings, state, focuses) // mutating, meh
-      // state = update(settings, state) // not sure
-
+      console.log('zoomToFit', state.position, state.target,  input)
+      let camera = state
+      const {bounds, transforms} = input
+      const offsetTargetAndPosition = cameraOffsetToEntityBoundsCenter({camera, bounds, transforms, axis: 2})
+      camera = Object.assign({}, state, offsetTargetAndPosition)
+      const phase2 = computeCameraToFitBounds({camera, bounds, transforms})
+      state.targetTgt = phase2.target
+      state.positionTgt = phase2.position
       return state
     }
 
+    //this is used for 'continuous updates' for things like spin effects, autoRotate etc
     function updateState (state) {
+      //i++
+      //textNode.nodeValue= 'foo'+i
+      //return state
       return Object.assign({}, state, update(settings, state))
     }
 
-    const updateFunctions = {applyZoom, applyRotation, updateState, applyFocusOn, zoomToFit, setProjection}
+    const updateFunctions = {
+      setProjection,
+      applyZoom,
+      applyRotation,
+      //applyFocusOn,
+      zoomToFit,
+      updateState
+    }
     const actions = {
       setProjection: projection$,
       applyZoom: zooms$,
       applyRotation: dragMoves$,
-      updateState: rate$,
-      applyFocusOn: focuses$,
-    zoomToFit: entityFocuses$}
+      //applyFocusOn: focuses$,
+      zoomToFit: entityFocuses$,
+      updateState: rate$
+    }
 
     const cameraState$ = model(camera, actions, updateFunctions)
 
@@ -105,9 +125,11 @@ export default function controlsStream (interactions, cameraData, focuses$, enti
 
   const cameraState$ = makeCameraModel()
 
-  return rate$.sample(x => x,
+  return cameraState$
+    .thru(limitFlow(33))
+  /*return rate$.sample(x => x,
     cameraState$
       .filter(x => x.changed)
       .merge(cameraState$.take(1))
-  )
+  )*/
 }
