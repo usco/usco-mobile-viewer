5329ceaf43c4936d9fa36f68cd1aef778d116ab3
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-11-24 13:18:02 +0100

    feat(prepPipeline): added regeneration of normals when the original ones are missing/broken

--- src/main/prepPipeline.js
+++ src/main/prepPipeline.js
@@ -1,12 +1,13 @@
 import computeBounds from '../common/bounds/computeBounds'
 import computeTMatrixFromTransforms from '../common/utils/computeTMatrixFromTransforms'
-import normals from 'angle-normals'
+import computeNormalsFromUnindexedPositions from '../common/utils/geometry/computeNormalsFromUnindexedPositions'
+import doNormalsNeedComputing from '../common/utils/geometry/doNormalsNeedComputing'
 
 // inject bounding box(& co) data
 export function injectBounds (entity) {
   const bounds = computeBounds(entity)
   const result = Object.assign({}, entity, {bounds})
-  //console.log('data with bounds', result)
+  // console.log('data with bounds', result)
   return result
 }
 
@@ -14,14 +15,17 @@ export function injectBounds (entity) {
 export function injectTMatrix (entity) {
   const modelMat = computeTMatrixFromTransforms(entity.transforms)
   const result = Object.assign({}, entity, {modelMat})
-  //console.log('result', result)
+  // console.log('result', result)
   return result
 }
 
-// inject object transformation matrix : costly : only do it when changes happened to objects
+// inject normals
 export function injectNormals (entity) {
   const {geometry} = entity
-  geometry.normals = geometry.cells && !geometry.normals ? normals(geometry.cells, geometry.positions) : geometry.normals
+  // FIXME: not entirely sure we should always recompute it, but we had cases of files with normals specified, but wrong
+  // let tmpGeometry = reindex(geometry.positions)
+  // geometry.normals = normals(tmpGeometry.cells, tmpGeometry.positions)
+  geometry.normals = doNormalsNeedComputing(geometry) ? computeNormalsFromUnindexedPositions(geometry.positions) : geometry.normals
   const result = Object.assign({}, entity, {geometry})
   return result
 }
