00ad3a7d76211834bdd8148edcbbfb8d4811a289
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-11-24 14:53:40 +0100

    refactor(): various cleanups, fleshing out of docstrings etc

--- src/common/mobilePlatforms/interface.js
+++ src/common/mobilePlatforms/interface.js
@@ -1,12 +1,9 @@
 import detectPlatform from './detector'
-// ugh , lack of dynamic loading ...
-// import { onLoadModelError as androidModelError, onLoadModelSuccess as androidModelSuccess, onBoundsExceeded as androidBoundsExceeded, onViewerReady as androidViewerReady } from './androidInterface'
-// import { onLoadModelError as IOSModelError, onLoadModelSuccess as IOSModelSuccess, onBoundsExceeded as IOSBoundsExceeded , onViewerReady as IOSViewerReady} from './iosInterface'
 import { makeAndroidInterface } from './androidInterface'
 import { makeIosInterface } from './iosInterface'
 
 export default function makeInterface () {
-  console.log('platform', detectPlatform())
+  //console.log('platform', detectPlatform())
   const platform = detectPlatform()
   const actions = platform === 'ios' ? makeIosInterface() : makeAndroidInterface()
   return actions
--- src/main/sideEffects/appMetaDataDriver.js
+++ src/main/sideEffects/appMetaDataDriver.js
@@ -1,9 +1,13 @@
 import {just} from 'most'
 
+/**
+ * very simple driver/side effect expose the app's version (from the package.json file)
+ * @return {Observable} observable with a single 'version' field as value
+*/
 export default function appMetaDataDriver () {
-  let json = require('../../package.json')
-  let appMetadata$ = just({
+  const json = require('../../../package.json')
+  const appMetadata$ = just({
     version: json.version
-  })
+  }).multicast()
   return appMetadata$
 }
