eb72e66670aa1ea4898b4094b4e8e0fd0eabd381
Mark 'kaosat-dev' Moissette <kaosat.dev@gmail.com>
2016-12-13 14:24:58 +0100

    refactor(utils): removed 'common' folder , now all under 'utils'

--- src/main/state.js
+++ src/main/state.js
@@ -1,5 +1,5 @@
-import { model } from '../common/utils/modelUtils'
-import isObjectOutsideBounds from '../common/utils/printing/isObjectOutsideBounds'
+import { model } from '../utils/modelUtils'
+import isObjectOutsideBounds from '../utils/printing/isObjectOutsideBounds'
 import { combine, combineArray, merge, just } from 'most'
 
 export function makeEntitiesModel (actions) {
